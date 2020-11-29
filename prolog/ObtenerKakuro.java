cbb2a69b813b14f6b95d5dec681a1c2fcc571f7f
randycj <randyconejo@gmail.com>
2020-11-29 17:38:48 -0600

    se puede verificar estado del kakuro desde prolog con java

--- prolog/ObtenerKakuro.java
+++ prolog/ObtenerKakuro.java
@@ -1,14 +1,19 @@
 import org.jpl7.Query;
 import org.jpl7.Term;
+
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
+        //Siempre tiene que hacer esta consulta
         String t1 = "consult('/home/randycj/Escritorio/Lenguajes/Kakuro/prolog/Proyecto03.pl')";
         Query q1 = new Query(t1);
         System.out.println(t1 + " " + (q1.hasSolution() ? "verdadero" : "fallo")); //mostrara mensaje  si hay o no conexion
 
+        //Esto es para generar el kakuro aleatorio
         String t2 = "generarKakuro(X, Y)";
         Query q2 = new Query(t2);
         java.util.HashMap solucion;
@@ -16,9 +21,13 @@ public class Main {
         Term kakuroSolucion = (Term) solucion.get("X");
         Term kakuro = (Term) solucion.get("Y");
 
-        String tableroKakuroS[][] = new String[9][9];
-        String tableroKakuro[][] = new String[9][9];
+        //las ultimas dos listas son para enviarlas de vuelta a prolog para verificar
+        String tableroKakuroS[][] = new String[9][9];//matriz con solucion
+        String tableroKakuro[][] = new String[9][9];//matriz sin solucion
+        String listaKakuroS[] = new String[81];//lista con solucion
+        String listaKakuro[] = new String[81];//lista sin solucion
 
+        //se encarga de rellenar las matrices y listas con los kakuros generados aleatoriamente
         int i = 0;
         int j = 0;
         while(kakuroSolucion.arity() == 2) {
@@ -31,12 +40,17 @@ public class Main {
 
             if (elemento1.substring(0, 1).equals("'")) {
                 elemento1 = elemento1.substring(4, elemento1.length()-1);
+                StringTokenizer tokens = new StringTokenizer(elemento1, ",");
+                String str1 = tokens.nextToken();
+                String str2 = tokens.nextToken().substring(1);
+                elemento1 = str1 + "/" + str2;
+                elemento2 = elemento1;
             }
-            if (elemento2.substring(0, 1).equals("'")) {
-                elemento2 = elemento2.substring(4, elemento2.length()-1);
-            }
+
             tableroKakuroS[i][j] = elemento1;
             tableroKakuro[i][j] = elemento2;
+            listaKakuroS[i*9+j] = elemento1;
+            listaKakuro[i*9+j] = elemento2;
             kakuroSolucion = kakuroSolucion.arg(2);
             kakuro = kakuro.arg(2);
 
@@ -63,6 +77,27 @@ public class Main {
             }
             System.out.println("|");
         }
+
+        //Convierte listas en strings, para enviarlas a prolog en verificar
+        String strKakuroS = Arrays.asList(listaKakuroS).toString();
+        String strKakuro = Arrays.asList(listaKakuro).toString();
+        //System.out.println(strKakuroS);
+        //System.out.println(strKakuro);
+
+        //se llama a prolog para verificar el kakuro que se este jugando
+        //aqui se deberia convertir la matriz en funcion y luego es string
+        //strKakuro es el kakuro que se esta solucionando, strKakuroS tiene la solucion
+        String t3 = "verificarKakuro(" + strKakuro + ", " + strKakuroS + ", X, Y)";
+        Query q3 = new Query(t3);
+
+        //obtiene los errores y casillas blancas del juego que se le mande
+        java.util.HashMap validacionKakuro;
+        validacionKakuro = (HashMap) q3.oneSolution();
+        String errores = validacionKakuro.get("X").toString();
+        String casillasBlancas = validacionKakuro.get("Y").toString();
+
+        System.out.println("Casillas sin rellenar: " + casillasBlancas);
+        System.out.println("Total de errores: " + errores);
 
     }
 }
